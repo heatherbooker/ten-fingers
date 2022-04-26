11cb528fb10f6ebe5e08232c5ac6deb8e9b96db2
hboo <heather.booker@hotmail.com>
2022-04-26 16:35:34 -0400

    Small fixes - refactor, add instructions, css

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const fingers = ['thumb', 'index', 'middle', 'ring', 'pinky'];
 
+function showDaysSinceClicked(id, lastClicked) {
+  const lastClickedDate = new Date(lastClicked);
+  const today = new Date();
+  const millisecondsPerDay = 1000*60*60*24;
+  const timeDifference = today.getTime() - lastClickedDate.getTime();
+  const daysDifference = Math.round( timeDifference / millisecondsPerDay );
+
+  document.getElementById(id + 'Text').innerHTML = daysDifference;
+}
+
+function setup(id) {
+  document.getElementById(id).onclick = (event) => {
+    const date = new Date();
+    localStorage.setItem(id, date);
+    event.currentTarget.setAttribute('class', 'clicked');
+  }
+
+  const lastClicked = localStorage.getItem(id);
+  if (lastClicked) {
+    showDaysSinceClicked(id, lastClicked);
+  }
+}
+
 fingers.forEach(finger => {
   setup(finger + 'Left');
   setup(finger + 'Right');
-  function setup(id) {
-    document.getElementById(id).onclick = (event) => {
-      const date = new Date();
-      localStorage.setItem(id, date);
-      event.currentTarget.setAttribute('class', 'clicked');
-    }
-    const lastClicked = localStorage.getItem(id);
-    if (lastClicked) {
-      const lastClickedDate = new Date(lastClicked);
-      const today = new Date();
-      const millisecondsPerDay = 1000*60*60*24;
-      const timeDifference = today.getTime() - lastClickedDate.getTime();
-      const daysDifference = Math.round( timeDifference / millisecondsPerDay );
-      document.getElementById(id + 'Text').innerHTML = daysDifference;
-    }
-  }
 });
