04cd81348ca58496f52b5853323918c7d259819b
hboo <heather.booker@hotmail.com>
2022-04-26 16:10:17 -0400

    it works! mvp

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const fingers = ['thumb', 'index', 'middle', 'ring', 'pinky'];
 fingers.forEach(finger => {
   document.getElementById(finger).onclick = (event) => {
     const date = new Date();
-    date.setHours(0, 0, 0, 0);
     localStorage.setItem(finger, date);
-    console.log(event.currentTarget);
     event.currentTarget.setAttribute('class', 'clicked');
+  }
+  const lastClicked = new Date(localStorage.getItem(finger));
+  if (lastClicked) {
+    const today = new Date();
+    const millisecondsPerDay = 1000*60*60*24;
+    const timeDifference = today.getTime() - lastClicked.getTime();
+    const daysDifference = Math.round( timeDifference / millisecondsPerDay );
+    document.getElementById(finger + 'Text').innerHTML = daysDifference;
   }
 });
